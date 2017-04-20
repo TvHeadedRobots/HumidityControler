8b3c8ea73fa591e4dedb38a10f14dae2a9c933f9
TvHeadedRobots <pneusolematic@mac.com>
2017-04-20 08:17:58 -0400

    Added pot functionality
    Read pot and pass value to chk humidity

--- DHT11_example.ino.ino
+++ DHT11_example.ino.ino
@@ -4,6 +4,10 @@ dht DHT;
 
 #define DHT11_PIN 10
 int led_pin = 18;
+int pot_pin = 21;
+int potVal = 0;
+int humiditySet = 0;
+int loopCntr = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -18,17 +22,42 @@ void loop()
   if(chk != 0) {
     Serial.println(chk);
   }
- 
-  if (DHT.humidity >= 80){
+  
+  potVal = analogRead(pot_pin);
+  humiditySet = map(potVal, 0,255, 10,90);
+  Serial.print("Pot Val = ");
+  Serial.println(potVal);
+  Serial.print("Map Pot Val = ");
+  Serial.println(humiditySet);
+  
+  if(loopCntr = 0) {
+    loopCntr = 1;
+  }
+  if(loopCntr = 100) {
+    chkHumidity(humiditySet); //need to pass analogRead() val
+  }
+  else {
+    loopCntr++;
+  }
+  
+  delay(50);
+}
+
+//Humidity check function
+void chkHumidity(int h)
+{
+  if (DHT.humidity >= h){
     digitalWrite(led_pin, LOW);
+    Serial.print("Set val = ");
+    Serial.println(h);
     Serial.print("GT50 Humidity = ");
     Serial.println(DHT.humidity);
   }
   else {
     digitalWrite(led_pin, HIGH);
+    Serial.print("Set val = ");
+    Serial.println(h);
     Serial.print("LT50 Humidity = ");
     Serial.println(DHT.humidity);
   }
-  
-  delay(5000);
 }
